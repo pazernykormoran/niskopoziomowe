5b68604c34f6377f93448ed66747a09dcd44b409
Andrzej D <a.daniel@techocean.pl>
2020-06-25 16:55:50 +0200

    initial commit

---
+++ main2.cpp
@@ -0,0 +1,325 @@
+#include "Utils.h"
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+
+#include <opencv/cv.h>
+
+#include <iostream>
+#include <tensorflow/c/c_api.h>
+
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <thread>
+
+
+// #include "include/MvCameraControl.h"
+
+#include <stdio.h>
+#include <stdlib.h> // exit()
+#include <string.h> // strlen()
+#include <stdbool.h>
+
+#include <sys/socket.h> // socket()
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h> // inet_pton()
+
+
+/*
+-----------------------------TENSORFLOW--------------------------------------------------------------------------------------
+*/
+TF_Graph* graph_;
+TF_Output inputOp_;
+TF_Output outputOp_;
+TF_Tensor* inputTensor_;
+void* tensorData_;
+const std::vector<std::int64_t> input_dims_{1, 224, 224, 3};
+const int imgW_{224};
+const int imgH_{224};
+const int imgC_{3};
+const int imgS_{imgH_*imgW_*imgC_};
+
+bool configure(){
+  inputTensor_ = TF_AllocateTensor(
+          TF_FLOAT,
+          input_dims_.data(),
+          static_cast<int>(input_dims_.size()),
+          imgS_ * sizeof(float)
+  );
+  if (inputTensor_ == nullptr) {
+      std::cout << "Can't allocate tensor" << std::endl;
+      exit(1);
+  }
+
+  // Get pointer to input tensor data
+  tensorData_ = TF_TensorData(inputTensor_);
+  if (tensorData_ == nullptr) {
+      std::cout << "Can't get tensorData_" << std::endl;
+      TF_DeleteTensor(inputTensor_);
+      exit(1);
+  }
+
+  // Load tensorflow model
+  std::string modelFile = "models/correct/model.pb";
+  graph_ = utils::LoadGraphDef(modelFile.c_str());
+  if (graph_ == nullptr) {
+      std::cout << "Can't load graph" << std::endl;
+      return 0;
+  }
+
+  // Prepare input
+  inputOp_ = {TF_GraphOperationByName(graph_, "Placeholder"), 0};
+  if (inputOp_.oper == nullptr) {
+      std::cout << "Can't init input_op" << std::endl;
+      return 0;
+  }
+
+  // Prepare output
+  outputOp_ = {TF_GraphOperationByName(graph_, "loss"), 0};
+  if (outputOp_.oper == nullptr) {
+      std::cout << "Can't init output_op" << std::endl;
+      return 0;
+  }
+  return true;
+}
+
+bool predict(cv::Mat _image, bool& _result){
+        if (_image.cols != imgH_ || _image.rows != imgW_) {
+            cv::resize(_image, _image, cv::Size(imgW_, imgH_));
+        }
+
+
+        cv::namedWindow("edges",1);
+        cv::imshow("edges", _image);
+        cv::waitKey(200);
+
+
+        for (int i=0; i<imgS_; ++i) {
+            ((float*)tensorData_)[i] = (float)_image.data[i];
+        }
+        TF_Tensor* outputTensor = nullptr;
+        //TF_Tensor* outputTensor;
+        TF_Status* status = TF_NewStatus();
+        TF_SessionOptions* options = TF_NewSessionOptions();
+        TF_Session* sess = TF_NewSession(graph_, options, status);
+        TF_DeleteSessionOptions(options);
+        if (TF_GetCode(status) != TF_OK) {
+            TF_DeleteStatus(status);
+            return false;
+        }
+            // auto start = std::chrono::high_resolution_clock::now(); 
+        TF_SessionRun(sess,
+                      nullptr, // Run options.
+                      &inputOp_, &inputTensor_, 1, // Input tensors, input tensor values, number of inputs.
+                      &outputOp_, &outputTensor, 1, // Output tensors, output tensor values, number of outputs.
+                      nullptr, 0, // Target operations, number of targets.
+                      nullptr, // Run metadata.
+                      status // Output status.
+        );
+            // auto stop = std::chrono::high_resolution_clock::now(); 
+            // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+            // std::cout << "Czas miliseconds SESSION RUN: " << duration.count()/1000 << std::endl; 
+        if (TF_GetCode(status) != TF_OK) {
+            std::cout << "Error run session";
+
+            std::cout<<TF_GetCode(status)<<std::endl;
+            std::cout<<TF_Message(status)<<std::endl;
+        
+            TF_DeleteStatus(status);
+            return false;
+        }
+        TF_CloseSession(sess, status);
+        if (TF_GetCode(status) != TF_OK) {
+            std::cout << "Error close session";
+            TF_DeleteStatus(status);
+            return false;
+        }
+        TF_DeleteSession(sess, status);
+        if (TF_GetCode(status) != TF_OK) {
+            std::cout << "Error delete session";
+            TF_DeleteStatus(status);
+            return false;
+        }
+        const auto data = static_cast<float*>(TF_TensorData(outputTensor));
+        _result = true;
+        if (100*data[0] >= 100*data[1]) {
+            _result = false;
+        }
+        // std::cout << "Tensor::predict "<<" - Prediction: [[" << _result<<"]] Output vals: bad[[" << data[0] << "]], good[[" << data[1]<<"]]";
+        TF_DeleteTensor(outputTensor);
+        TF_DeleteStatus(status);
+
+            // stop = std::chrono::high_resolution_clock::now(); 
+            // duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start); 
+            // std::cout << "Czas miliseconds WSZYSTKO: " << duration.count()/1000 << std::endl; 
+
+        return true;
+
+}
+
+/*
+-----------------------------IMG ENCODER--------------------------------------------------------------------------------------
+*/
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+    std::string base64_chars = 
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      "abcdefghijklmnopqrstuvwxyz"
+      "0123456789+/";
+
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for(i = 0; (i <4) ; i++)
+            ret += base64_chars[char_array_4[i]];
+        i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for(j = i; j < 3; j++)
+        char_array_3[j] = '\0';
+
+        char_array_4[0] = ( char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+
+        for (j = 0; (j < i + 1); j++)
+        ret += base64_chars[char_array_4[j]];
+
+        while((i++ < 3))
+        ret += '=';
+
+    }
+
+    return ret;
+
+}
+
+std::string encode_image(cv::Mat image){
+    std::vector<uchar> buf;
+    cv::imencode(".jpg", image, buf);
+    auto *enc_msg = reinterpret_cast<unsigned char*>(buf.data());
+    std::string encoded = base64_encode(enc_msg, buf.size());
+    return encoded;
+}
+
+
+
+
+
+/*
+-----------------------------TCP CLIENT--------------------------------------------------------------------------------------
+*/
+struct sockaddr_in serwer =
+{
+    .sin_family = AF_INET,
+    .sin_port = htons( 10000 )
+};
+const char * ipAddress = "192.168.1.10";
+
+int tcpConnect(){
+
+    inet_pton( serwer.sin_family, ipAddress, & serwer.sin_addr );
+    const int s = socket( serwer.sin_family, SOCK_STREAM, 0 );
+    connect( s,( struct sockaddr * ) & serwer, sizeof( serwer ) );
+
+   
+    // while( recv( s, buffer, sizeof( buffer ), 0 ) > 0 )
+    // {
+    //     // puts( buffer );
+    //     // if( entireWebsidedLoaded( buffer ) )
+    //     // {
+    //     //     strcpy( buffer, "^]" ); /// exit character
+    //     //     send( s, buffer, strlen( buffer ), 0 );
+    //     //     break;
+    //     // }
+
+    //     std::cout<<"Receaved: "<<  buffer<<std::endl;
+    // }
+   
+    return s;
+}
+
+
+
+bool sendFrame(std::string imageString, const int &s){
+  char buffer[ imageString.length()+1 ];
+  strcpy(buffer,imageString.c_str());
+  send( s, buffer, strlen( buffer ), 0 );
+}
+
+bool tcpClose( const int &s){
+  shutdown( s, SHUT_RDWR );
+}
+
+
+/*
+-----------------------------MAIN--------------------------------------------------------------------------------------
+*/
+int main(int argc, char const* argv[]) {
+
+  std::cout<<"Test server... "<<std::endl;
+  const int s = tcpConnect();
+  bool sended = sendFrame("Message ssssssssssssssssssssending",s);
+  bool closed = tcpClose(s);
+
+  bool configured = configure();
+  std::cout<<"elo"<<std::endl;
+  // cv::VideoCapture cap("rtsp://192.168.1.51:554/ch0_0.h264");
+  cv::VideoCapture cap2(0);
+  // if(!cap.isOpened())  // check if we succeeded
+  //   return -1;
+  
+  if(!cap2.isOpened())  // check if we succeeded
+    return -1;
+
+
+  cv::Mat newframe;
+  std::string encoded_image;
+  while(true){
+  
+    cap2 >> newframe;
+    cv::Mat dst;
+    bool result;
+    predict(newframe,result);
+    std::cout<<"resutl: "<< result;
+    if(result){
+      std::cout<<" Sending photo to server...";
+      encoded_image = encode_image(newframe);
+      sendFrame(encoded_image,s);
+    }else{
+      std::cout<<" Sending photo to server...";
+      encoded_image = encode_image(newframe);
+      sendFrame(encoded_image,s);
+    }
+
+    // cv::namedWindow("edges",1);
+    // cv::imshow("edges", newframe);
+    // cv::waitKey(20);
+  }
+}
+
+
+
+
+
+      
+
+
---
+++ server/app.py
@@ -0,0 +1,76 @@
+import socket
+import sys
+
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address given on the command line
+server_address = ('192.168.1.10', 10000)
+print('starting up sever')
+sock.bind(server_address)
+sock.listen(1)
+
+
+# def read_mc(self):
+#     while self.acceptReadMc:
+#         try:
+#             # msg = 'Control Message from Server to Module Camera'  # For detection broken pipe
+#             msg = ''  # Control empty message for detection broken pipe
+#             self.connMc.sendall(msg.encode())
+#             data = self.connMc.recv(1048576)
+#             if data:
+#                 message = json.loads(data)
+#                 self.dequeMc.append(message)
+#                 if message['subtype'] == MessageSubtype.CAMERA_CONFIGURATION.value:
+#                     self.socket_bridge.sendCameraConfig(json.loads(base64.b64decode(message['payload']).decode()))
+#                 if message['subtype'] == 4:
+#                     self.socket_bridge.sendModelInfo(json.loads(base64.b64decode(message['payload']).decode()))
+#                 print("<< MC << Received message with id: " + str(message['id']))
+#                 # print(message)
+
+#         except socket.timeout:
+#             continue
+
+#         except BrokenPipeError as bre:
+#             print('Trying to read data from broken Module Camera socket connection !', bre)
+#             self.connections.remove(self.connMc)
+#             self.hasAllConn = False  # Start again waiting for broken connection
+#             self.acceptReadMc = False
+#             self.connMc = None
+#             self.socket_bridge.notify({'type': NotifType.WARNING.value, 'message': 'ModuleCamera not connected'})
+
+#         time.sleep(0.1)
+
+while True:
+    print("waiting for connection")
+    connection, client_address = sock.accept()
+    try:
+        print( 'client connected:', client_address)
+        while True:
+            data = connection.recv(16)
+            print("reseived", data)
+            if data:
+                connection.sendall(data)
+            else:
+                break
+            
+
+            msg_rcv = json.loads(data)
+
+            print('Type message rcv: ', str(msg_rcv['type']))
+            print('Id message rcv: ', str(msg_rcv['id']))
+
+            if msg_rcv['type'] == MessageType.CAMERA.value:
+                print('Recognized type CAMERA: ', str(msg_rcv['type']))
+                print('Subtype', str(msg_rcv['subtype']))
+                self.socket_bridge.notify({'type': NotifType.SUCCESS.value, 'message': 'ModuleCamera connected'})
+                self.connections.append(connection)
+                self.connMc = connection
+                self.acceptReadMc = True
+                thread = threading.Thread(target=self.read_mc)
+                thread.start()
+
+    finally:
+        connection.close()
+    
